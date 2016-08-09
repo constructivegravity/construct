ca559cff3e2c3af86a3024e965f44a6a73a26714
Jonas Schneider <jonas.schneider1@web.de>
2016-08-09 14:45:05 +0200

    Updated term ordering after substitution.

--- test/tensor/substitution.cpp
+++ test/tensor/substitution.cpp
@@ -13,7 +13,7 @@ SCENARIO("Substitution", "[substitution]") {
         subst.Insert(Construction::Tensor::Scalar("e_1"), -Construction::Tensor::Scalar("e_2"));
 
         auto substituted = subst(tensor);
-        REQUIRE(substituted.ToString() == "e_2 * (-\\gamma_{ab}\\gamma_{cd} + \\gamma_{ac}\\gamma_{bd} + \\gamma_{ad}\\gamma_{bc})");
+        REQUIRE(substituted.ToString() == "e_2 * (-\\gamma_{ac}\\gamma_{bd} - \\gamma_{ad}\\gamma_{bc} + \\gamma_{ab}\\gamma_{cd})");
 
     }
 
