624ecf7486e6364ffae93af66ebfee9512738ec7
Florian Wolz <FlorianWolz@gmx.net>
2017-05-23 12:42:02 +0200

    Merged with master

--- lib/tensor/fraction.hpp
+++ lib/tensor/fraction.hpp
@@ -29,6 +29,7 @@ namespace Construction {
                 T tmp;
                 num1 = (num1 > 0) ? num1 : -num1;
                 num2 = (num2 > 0) ? num2 : -num2;
+
                 while (num1 > 0) {
                     tmp = num1;
                     num1 = num2 % num1;
@@ -41,6 +42,7 @@ namespace Construction {
                 if (numerator == 0) return;
 
                 T g = gcd(numerator, denominator);
+
                 numerator /= g;
                 denominator /= g;
 
